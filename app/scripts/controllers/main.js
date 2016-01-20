f906094515727fabd514605746566680655f0030
toniherse86 <toniherse86@gmail.com>
2016-01-20 23:17:46 +0100

    bezoya

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bezoyaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bezoyaApp
+ */
+angular.module('bezoyaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
